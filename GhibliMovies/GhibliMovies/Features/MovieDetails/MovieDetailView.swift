6571d74ada601c3ed4a0d6627a4bca8f84e9a464
JoaoSilva <jplimadev@gmail.com>
2026-02-15 20:17:59 -0300

    chore: create MovieDetailView placeholder #8

---
+++ GhibliMovies/GhibliMovies/Features/MovieDetails/MovieDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailView.swift
+//  GhibliMovies
+//
+//  Created by Joao Paulo Lima Silva on 15/02/26.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailView()
+}
